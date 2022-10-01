4fea3e7fc1c25a05f3a7273bf08c0d238dc80744
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-10-01 15:39:36 +0200

    Menu Interativo

--- JavaScript/EXERCICIOS/exer07/menu.js
+++ JavaScript/EXERCICIOS/exer07/menu.js
@@ -1,48 +1,26 @@
-alert(
-  "Welcome to our restaurant!\nwe will offer you some of our specialties, of Brazilian food, make your choice!"
-);
-alert(
-  "----MENU----\n\n1° strogonoff de camarão\n2° Vaca atolada\n3° Frango com quiabo\n4° Feijão tropeiro completo\n5° I'm not hungry"
-);
-let choice = Number(prompt("Enter your order number!"));
 
-while (choice !== 5) {
-  alert("Thank you for your school have a good appetite.");
-  alert("Want anything else, choose from our menu");
-  alert(
-    "----MENU----\n\n1° strogonoff de camarão\n2° Vaca atolada\n3° Frango com quiabo\n4° Feijão tropeiro completo\n5° Nao tenho fome."
-  );
-  let choice = Number(prompt("Enter your order number."));
-  if (
-    choice !== 5 &&
-    choice !== 4 &&
-    choice !== 3 &&
-    choice !== 2 &&
-    choice !== 1
-  ) {
-    alert("Error choose a valid number");
-  } else if (choice == 5) {alert("Thank you and come back often.");
-    break;
+let opcao = "5"
+
+do{
+  opcao = prompt("Welcome to our restaurant!\nwe will offer you some of our specialties, make your choice!\n\n----MENU----\n1° strogonoff de camarão\n2° Vaca atolada\n3° Frango com quiabo\n4° Feijão tropeiro completo\n5° I'm not hungry.")
+  switch (opcao) {
+    case "1":
+      alert("Good school, we'll bring you your shrimp stroganoff in minutes");
+      break;
+    case "2":
+      alert("Good school, let's bring your Vaca atolada in minutes");
+      break;
+    case "3":
+      alert("Good school, let's bring your Frango com quiabo in minutes");
+      break;
+    case "4":
+      alert("Good school, let's bring your seu Feijão tropeiro in minutes");
+      break;
+    case "5":
+      alert("Thank you for your visit and come back often.");
+      break;
+      default:
+        alert("Valid option please enter a valid number.");
   }
 
-}
-
-
-/*
-alert("Bem vindo ao nosso restourante!\nvamos te ofereçer algumas das nossas especialidades, fassa sua escolha!")
- alert("----MENU----\n\n1° strogonoff de camarão\n2° Vaca atolada\n3° Frango com quiabo\n4° Feijão tropeiro completo\n5° Nao tenho fome.");
-let escolha = Number(prompt("Digite o numero do seu pedido!"));
-
-while(escolha !== 5){
-    
-    alert("Obrigado pela sua escola tenha um bom apetite.");
-     alert("Deseja mais aguma coisa, escolha no nosso menu");
-    alert(
-      "----MENU----\n\n1° strogonoff de camarão\n2° Vaca atolada\n3° Frango com quiabo\n4° Feijão tropeiro completo\n5° Nao tenho fome."
-    );
-    let escolha = Number(prompt("Digite o numero do seu pedido."));
-    if(escolha == 5){
-       break
-    }
-}alert("Obrigado e volte sempre.");
-*/
+} while(opcao !==5)
