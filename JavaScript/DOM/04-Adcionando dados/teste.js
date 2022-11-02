9ea82e17a110a9a48afdab19315ad45e21a55315
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-02 19:48:18 +0100

    Remover Elementos Dom

--- JavaScript/DOM/04-Adcionando dados/teste.js
+++ JavaScript/DOM/04-Adcionando dados/teste.js
@@ -8,6 +8,7 @@ nomeCompleto = jogador + " " + jogador2;
 const ul = document.createElement("ul");
 
 const li = document.createElement("li");
+li.id = "li-nome"
 li.innerText = nomeCompleto; //aqui estou pegando o valor da caixa enput
 ul.appendChild(li);
 span.appendChild(ul);
@@ -16,8 +17,3 @@ document.querySelector("#ijogo").value = ""; //limpa o input depois que envia od
 document.querySelector("#ijogo2").value = "";
 }
 
-function remover(){
-   let span = document.getElementById("list-span");
-   let usuario = document.getElementsByClassName(".nome", ".sobrenome")
-   usuario= document.replaceChild("span")
-}
