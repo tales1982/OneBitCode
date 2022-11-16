4f591c2af8679d5f9d31846949d4b53aa2c4ae9d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-16 20:56:17 +0100

    Desafio 01

--- 30-Desafios-EM-Javascript/Desafio-01/index.js
+++ 30-Desafios-EM-Javascript/Desafio-01/index.js
@@ -7,21 +7,16 @@ for(let i in n1){
 
 console.log(soma / n1.length)
 */
-let resultado = document.querySelector("#res")
-
+let resultado = document.querySelector("#res");
 
 // Somando as notas do array
 let n1 = Number(prompt("Digite a primeira nota do cadete."));
 let n2 = Number(prompt("Digite a segunda nota do cadete."));
 let n3 = Number(prompt("Digite a terceira nota do cadete."));
 let n4 = Number(prompt("Digite a quarta nota do cadete."));
-let soma = 0
 
-let notas =[n1, n2, n3, n4]
+let notas = [n1, n2, n3, n4];
+const somaTotal = notas.reduce((a, b) => a + b); //usando o método reduce com a função arrow simples
+console.log(somaTotal);
 
-for(let i  in notas){
-    soma += notas[i]
-   
-} 
-
-resultado.innerHTML = `A media do cadete é ${soma / notas.length}`
+resultado.innerHTML = `A media do cadete é ${somaTotal / notas.length}`;
