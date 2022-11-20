fd82bbd682f52db38d15265512d6a1ca180dd433
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-20 21:04:30 +0100

    Exercicios

---
+++ 30-Desafios-EM-Javascript/01-FACIL/Desafio-04/main.js
@@ -0,0 +1,10 @@
+//const nomeVelho = prompt("Digite nome da pessoa mais velha");
+const idadeVelho = 40//Number(prompt("Digite a idade da pessoa mais velha"));
+
+//const nomeNovo = prompt("Digite o nome da pessoa mais nova");
+const idadeNovo = 25//Number(prompt("Digite a idade da pessoa mais nova"));
+
+const somaIdade = idadeVelho + idadeNovo
+const diferencaIdade = idadeVelho - idadeNovo;
+
+console.log(diferencaIdade)
