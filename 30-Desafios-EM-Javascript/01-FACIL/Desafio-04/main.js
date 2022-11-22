73c4fb6292d626e6b2e86000ec5e7d2d88fd01d5
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-22 21:39:50 +0100

    Desafio 04

--- 30-Desafios-EM-Javascript/01-FACIL/Desafio-04/main.js
+++ 30-Desafios-EM-Javascript/01-FACIL/Desafio-04/main.js
@@ -1,10 +1,13 @@
-//const nomeVelho = prompt("Digite nome da pessoa mais velha");
-const idadeVelho = 40//Number(prompt("Digite a idade da pessoa mais velha"));
+const nomeVelho = prompt("Digite nome da pessoa mais velha");
+const idadeVelho = Number(prompt(`Digite a idade de ${nomeVelho}`));
 
-//const nomeNovo = prompt("Digite o nome da pessoa mais nova");
-const idadeNovo = 25//Number(prompt("Digite a idade da pessoa mais nova"));
+const nomeNovo = prompt("Digite o nome da pessoa mais nova");
+const idadeNovo = Number(prompt(`Digite a idade de ${nomeNovo}`));
 
 const somaIdade = idadeVelho + idadeNovo
 const diferencaIdade = idadeVelho - idadeNovo;
 
-console.log(diferencaIdade)
+alert(`  A diferença de idade de ${nomeVelho} e ${nomeNovo} é ${diferencaIdade} anos.
+    O ${nomeVelho} é o mais velho. e o ${nomeNovo} é mais novo.
+    Voçes tem ${diferencaIdade} anos de diferença.
+`)
