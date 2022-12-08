96a0e28dfb4af99f97a2d89c74523d8be0ef544d
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-08 22:29:13 +0100

    ES -Modules

---
+++ JS-MODERNO/008-ES-Modules/index.js
@@ -0,0 +1,6 @@
+import {label,input, br,nome} from "./fuction.js "
+
+console.log(label({for:"fullname", textContent: "Nome Completo"}))
+console.log(input({id: "fullname", name: "fullname", placeholder: "Digite seu nome completo"}));
+console.log(br());
+console.log(nome)
