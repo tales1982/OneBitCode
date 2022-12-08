bb7a1dbf4f25dedaac9b94b157d71c7356966cd5
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-08 21:03:05 +0100

    CommonJS

---
+++ JS-MODERNO/007-CommonJS/index.js
@@ -0,0 +1,8 @@
+const render = require("./render")//Aqui estou importando meu codigo do outro arquivo que esta separado salvo em outro arquivo.
+const store = require("./store")//Aqui estou importando meu codigo do outro arquivo que esta separado salvo em outro arquivo.
+
+console.log("iniciar aplicaçao")
+render()
+store()
+
+console.log("termina aplicaçao")
---
+++ JS-MODERNO/007-CommonJS/store.js
@@ -0,0 +1,5 @@
+function store(){
+    console.log("teste Store")
+}
+
+module.exports = store//Estou exportando minha funcao para o arquivo principal
