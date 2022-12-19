762341b2d44aa18337798ddeb8dcf5bf20cec52e
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-19 23:36:58 +0100

    usando o webpack

--- JS-MODERNO/014-Webpack/src/js/index.js
+++ JS-MODERNO/014-Webpack/src/js/index.js
@@ -1,4 +1,6 @@
 import '../css/style.scss';
+import img from '../assets/gif.gif';
+import img2 from "../assets/img.png";
 
 const textComponent = ()=>{
     const elH1 = document.createElement("h1")
@@ -7,4 +9,17 @@ const textComponent = ()=>{
     return elH1 
 }
 
+const imgComponet = ()=>{
+    const elImg = new Image(600, 600);
+    elImg.src = img
+    return elImg
+}
+const imgComponet2 = () => {
+  const elImg = new Image(800, 600);
+  elImg.src = img2;
+  return elImg;
+};
+
+document.body.appendChild(imgComponet());
+document.body.appendChild(imgComponet2());
 document.body.appendChild(textComponent());
