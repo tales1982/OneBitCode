8eaaa718c17306d0e9bd89affa46bd5ae0f5a34b
tales1982 <tales_lima_1982@hotmail.com>
2023-06-22 19:50:43 +0200

    Interface vers Classe

--- Typescrpit/10-interface_X_classe/dist/index.js
+++ Typescrpit/10-interface_X_classe/dist/index.js
@@ -13,3 +13,10 @@ class Garage {
 let cliente;
 cliente = new Garage("Ferrari", "Modelo", 2022, "Vermelho");
 console.log(cliente);
+let cliente2 = {
+    nome: "Tales",
+    conta: 22446688,
+    saldo: 5.000,
+    bloqueado: false,
+};
+console.log(`O cliente ${cliente2.nome} possui o numero de conta ${cliente2.conta}, seu saldo e de ${cliente2.saldo},sua conta esta ${cliente2.bloqueado ? "Bloqueado" : "Desbloqueada"}`);
--- Typescrpit/10-interface_X_classe/src/index.ts
+++ Typescrpit/10-interface_X_classe/src/index.ts
@@ -23,3 +23,20 @@ interface Voiture {
   cliente = new Garage("Ferrari", "Modelo", 2022, "Vermelho");
   console.log(cliente);
   
+
+  interface Banco{
+    nome:string
+    conta:number
+    saldo:number
+    bloqueado:boolean
+  }
+
+
+ let cliente2: Banco ={
+  nome: "Tales",
+  conta: 22446688,
+  saldo: 5.000,
+  bloqueado: false,
+ }
+
+ console.log(`O cliente ${cliente2.nome} possui o numero de conta ${cliente2.conta}, seu saldo e de ${cliente2.saldo},sua conta esta ${cliente2.bloqueado ? "Bloqueado": "Desbloqueada"}`)
